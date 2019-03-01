cedc947bb591776350e0746903c2696e4cdfe601
Alex Liberzon <alexlib@tauex.tau.ac.il>
2019-03-01 11:53:58 +0200

    Create isrgb.m
    ISRGB was obsoleted by Mathworks
    I recreated a workaround that basically checks if the image has 3 dimensions

---
+++ MatPIV 1.7/src/isrgb.m
@@ -0,0 +1,8 @@
+function y = isrgb(x)
+%ISRGB Return true for RGB image.
+%   ISRGB has been removed.
+
+%   Copyright 1993-2011 The MathWorks, Inc.
+
+% error(message('images:removed:functionNoReplacement','ISRGB'))
+y = (ndims(x) == 3);
